040439c865f2ff0a10bafa804597e2a256cbaa2f
Casey Friday <casey@friday-next.com>
2021-03-23 12:15:37 -0500

    Consolidating API calls to one private function

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -24,22 +24,7 @@ class WeatherController extends AppController
      */
     public function view()
     {
-//        if (!$path) {
-//            return $this->redirect('/');
-//        }
-//        if (in_array('..', $path, true) || in_array('.', $path, true)) {
-//            throw new ForbiddenException();
-//        }
-//        $page = $subpage = null;
-//
-//        if (!empty($path[0])) {
-//            $page = $path[0];
-//        }
-//        if (!empty($path[1])) {
-//            $subpage = $path[1];
-//        }
-//        $this->set(compact('page', 'subpage'));
-
+        /******** IP ADDRESS ********/
         // Get user's IP address as a default
         //$user_ip = $this->request->clientIp(); // TODO: will use in production
         //$this->set('user_ip', $user_ip);       // TODO: will use in production
@@ -47,39 +32,40 @@ class WeatherController extends AppController
         $this->set('user_ip', $user_ip);
 
         // Grab lat/lon coords from IP address
-        $user_coords = $this->getUserCoords($user_ip);
-        //$this->set(compact('user_coords'));
+        $args = array(
+            'url' => 'https://freegeoip.app/json/',
+            'url_params' => $user_ip
+        );
+        $response = $this->apiCall($args);
+        // set IP to 'null' if unsuccessful
+        $user_coords = $response ? array($response['latitude'], $response['longitude']) : null;
         $this->set('user_coords', $user_coords);
 
-        // Get current weather for those lat/lon coords
-        $weather_arr = $this->getWeather($user_coords);
-        $this->set('weather', $weather_arr);
+        /******** WEATHER ********/
+        // Get list of closest locations for the previously attained IP coordinates
+        $args = array(
+            'url' => 'https://www.metaweather.com/api/location/search/?lattlong=',
+            'url_params' => $user_coords[0] .",". $user_coords[1]
+        );
+        $locations = $this->apiCall($args);
+        // Query for weather at first location in list
+        $args = array(
+            'url' => 'https://www.metaweather.com/api/location/',
+            'url_params' => $locations[0]['woeid']
+        );
+        $weather = $this->apiCall($args);
+        print_r($weather);die();
+        $this->set('weather', $locations[0]['woeid']);
 
         // Render the weather view template
         return $this->render('weather');
     }
 
-
-//    public function view($id = null)
-//    {
-//
-//        return $this->render('weather');
-//
-//    }
-
-    private function getUserCoords($user_ip): array
+    private function apiCall($args)
     {
-        /**
-         * Latitude / Longitude Coordinates from IP
-         *
-         * This function will return [lat, lon] when provided an IP address
-         *
-         * @link https://freegeoip.app
-         */
-
         $curl = curl_init();
         curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://freegeoip.app/json/" . $user_ip,
+            CURLOPT_URL => $args['url'] . $args['url_params'],
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
@@ -99,11 +85,10 @@ class WeatherController extends AppController
 
         if ($err) {
             echo "cURL Error #:" . $err;
+            return false;
         } else {
-            // The call was successful, sending back coords
-            $response_arr = json_decode($response, true);
-            $response = array($response_arr['latitude'], $response_arr['longitude']);
-            return $response;
+            // The call was successful, return the response
+            return json_decode($response, true);
         }
     }
 
@@ -132,7 +117,7 @@ class WeatherController extends AppController
         if ($err) {
             echo "cURL Error #:" . $err;
         } else {
-            // The call was successful, sending back weather
+            // The call was successful, use closest location (first in list) to find weather
             $response_arr = json_decode($response, true);
 
             return $response_arr;
--- templates/Weather/weather.php
+++ templates/Weather/weather.php
@@ -4,23 +4,37 @@
         <input type="text" class="form-control mb-0" id="ip-entry" aria-label="ip-address-entry" aria-describedby="inputGroup-sizing-lg">
     </div>
     <div class="container">
-        <div class="row justify-content-around">
-            <div class="card w-25 gx-0">
+        <div class="row justify-content-center">
+            <div class="card w-25 gx-0 mx-2">
                 <div class="card-header text-center">Latitude</div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item text-center mb-0"><?= $user_coords[0] ?></li>
                 </ul>
             </div>
-            <div class="card w-25 gx-0">
+            <div class="card w-25 gx-0 mx-2">
                 <div class="card-header text-center">Longitude</div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item text-center mb-0"><?= $user_coords[1] ?></li>
                 </ul>
             </div>
         </div>
+        <div class="row justify-content-center">
+            <div id="weather-details" class="card col px-0 mx-1" style="width: 20%;">
+                <div class="card-header text-center">
+                    Weather for <?= $user_ip ?>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item high-low text-center">
+                        <span class="text-primary"> °F</span> / <span class="text-danger"> °F</span>
+                        <br />
+                        <img src="" alt="Weather Icon" title="" width="50" height="50">';
+                    </li>
+                    <li class="list-group-item details"></li>
+                </ul>
+            </div>
+        </div>
         <?php
 
-    echo "Latitude: " . $user_coords[0] . ", Longitude: " . $user_coords[1] . '<br>';
     echo "Your IP: " . $user_ip . '<br>';
     echo "Weather info: ";
     print_r($weather); ?>
